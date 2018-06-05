16deec7d77c4dbcda1a4ed8f3988c35b8774f761
12201971 <12201971@facin.portoalegre.pucrsnet.br>
2018-06-04 22:20:01 -0300

    Communication working

--- mutualExclusion/src/Process.java
+++ mutualExclusion/src/Process.java
@@ -1,142 +1,144 @@
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 class Process {
 
-    private static String destineIp;
     private static int destinePort;
     private static int myPort;
-    private static int waitTime;
     private static String token;
-    private static DatagramSocket clientSocket;
-    private static byte[] receiveData = new byte[1024];
-    
-    
 
     public Process() {
-        
+
     }
 
     public static void main(String args[]) throws Exception {
-        destineIp = "127.0.0.1";
-        token = null;
+        token = "1234";
         myPort = 9872;
-   
-        clientSocket = new DatagramSocket(myPort);
-        
-        if (token != null){
-            tokenReceived(token);    
-        }
-        
+//        token = null;
+//        myPort = 9871;
+
         updateDestinePortOf(myPort);
+        System.out.println("dest" + destinePort);
         startListener();
+        startReadFile();
+        if (token != null) {
+            tokenReceived(token);
+        }
     }
     
-    
+    private static void startReadFile() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    readFile();
+                } catch (IOException ex) {
+                    System.out.println("Erro: " + ex.getMessage());
+                }
+            }
+        }, 1000);
+    }
+
     //CONTROL RESULTS FILE METHODS
-    private static void readFile() throws IOException{
+    private static void readFile() throws IOException {
+        if (!hasToken()) {
+            return;
+        }
+
         BufferedReader br = new BufferedReader(new FileReader("text.txt"));
         try {
-            
+
             String currentLine, lastLine = "";
-            
-            while((currentLine = br.readLine()) != null){
+
+            while ((currentLine = br.readLine()) != null) {
                 lastLine = currentLine;
             }
-            
-            if (lastLine != ""){
+
+            if (lastLine != "") {
                 System.out.println("last line: " + lastLine);
                 executeOperation(lastLine);
             }
-            
-        }catch (Exception e){
-            System.out.println("error reading the file");
+
+        } catch (Exception e) {
+            System.out.println("error reading the file xxxx");
         } finally {
             br.close();
         }
     }
-    
-    private static void executeOperation(String line){
+
+    private static void executeOperation(String line) {
         System.out.println("executing operation");
-        
+
         saveOperation(line);
     }
-    
-    private static void saveOperation(String line){
+
+    private static void saveOperation(String line) {
         System.out.println("saving operation");
-        
+
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("filename"))) {
-            
+
             bw.write(line);
         } catch (IOException e) {
             System.out.println("error writing on file");
             e.printStackTrace();
         }
     }
-    
+
     //CONTROL PROCESS FILE METHODS
-    //THIS METHOD SHOULD BE ALWAYS RUNNING ON A SECONDARY THREAD
-       private static void updateDestinePortOf(int port) throws IOException{
+    private static void updateDestinePortOf(int port) throws IOException {
+//        if(!hasToken()) {
+//            return;
+//        }
         BufferedReader br = new BufferedReader(new FileReader("process.txt"));
         try {
-            
+
             String firstLine = null;
             boolean isFirstLine = true;
-            
+
             String currentLine, nextLine;
-            while((currentLine = br.readLine()) != null){
-                
-                if(isFirstLine){
+            while ((currentLine = br.readLine()) != null) {
+
+                if (isFirstLine) {
                     firstLine = currentLine;
                     isFirstLine = false;
                 }
-                
+
                 int possiblePort = Integer.parseInt(currentLine);
-                if (possiblePort == port){
-                    
+                if (possiblePort == port) {
+
                     nextLine = br.readLine();
-                    if (nextLine != null){
-                        destinePort = Integer.getInteger(currentLine);
-                    }else{
-                        destinePort = Integer.getInteger(firstLine);
-                    } 
-                    
+                    if (nextLine != null) {
+                        destinePort = Integer.parseInt(nextLine);
+                    } else {
+                        destinePort = Integer.parseInt(firstLine);
+                    }
+
                     break;
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error reading the file");
         } finally {
             br.close();
         }
     }
-           
+
     //CONTROL TOKEN METHODS
-    private static boolean sendMessage(String message) throws Exception {
-            byte[] sendData = message.getBytes();
-            InetAddress destineAddress = InetAddress.getByName(destineIp);
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, destineAddress, destinePort);
-            clientSocket.send(sendPacket);
-            return true;
-    }
-    
     private static void sendToken() throws Exception {
         String msg = "1234";
-        System.out.println("port: " + myPort + " sending token");
+        System.out.println("port: " + myPort + " sending token" + destinePort);
         //sendMessage(msg);
-        
-        Socket cliente = new Socket(destineIp, destinePort);
-        ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-        saida.flush();
-        saida.writeObject(msg);
-        saida.close();
-        cliente.close();
+        try (Socket cliente = new Socket("localhost", destinePort)) {
+            ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+            saida.flush();
+            saida.writeObject(msg);
+            saida.close();
+        }
         token = null;
     }
 
@@ -151,33 +153,31 @@ class Process {
                 try {
                     // Instancia o ServerSocket ouvindo a porta 12345
                     ServerSocket servidor = new ServerSocket(myPort);
-                    while(true) {
+                    while (true) {
+                        System.out.println("*************");
                         Socket cliente = servidor.accept();
+                        System.out.println("*************");
                         ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-                        
+
                         String msg = (String) entrada.readObject();
                         System.out.println(msg + "received on port: " + myPort);
                         System.out.println("*************");
-                        
+
                         tokenReceived(msg);
-                        
+
                         entrada.close();
                         cliente.close();
-                  }  
-                }   
-                catch(Exception e) {
-                   System.out.println("Erro: " + e.getMessage());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Erro: " + e.getMessage());
                 }
             }
         }).start();
     }
-    
+
     private static void tokenReceived(String msg) throws Exception {
-       token = msg;
-       
-       //readFile();
-       TimeUnit.MINUTES.sleep(1);
-       
-       sendToken();
+        token = msg;
+
+        sendToken();
     }
 }
